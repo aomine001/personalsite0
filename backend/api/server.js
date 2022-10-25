26437f7f0cb630d662b0707dfa92806436e8bbdb
aomine001 <77345354+aomine001@users.noreply.github.com>
2022-10-25 19:11:49 +0200

    Databases

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const env = require('dotenv').config();
+const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 const App = express();
 
 let api_port = process.env.api_port;
+
+let create_db_contents = false;
+if (!fs.existsSync('./users.db')){
+    create_db_contents = true;
+}
+
+let Database = new sqlite3.Database('users.db', (err) => {
+    if (err)
+        console.log('There was error during loading/creating database! :(');
+    else {
+        if (create_db_contents){
+            let sql = `
+            CREATE TABLE users (
+                id varchar(255),
+                username varchar(56),
+                password varchar(255),
+                email varchar(1024)
+            );
+            `;
+
+            Database.exec(sql, (err) => {
+                console.log('Created new database!');
+            });
+        }
+
+        console.log('Database finished loading/creating!');
+    }
+});
 
 // Middlewares: 
 
